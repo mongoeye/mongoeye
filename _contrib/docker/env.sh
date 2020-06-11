5619c2e50dc68b6bf8c5527e377e3bf07ffac8b4
chakrapani velugubantla <chakrapani_velugubantla@cable.comcast.com>
2020-06-11 17:33:51 -0400

    updated with docker scripts

--- _contrib/docker/env.sh
+++ _contrib/docker/env.sh
@@ -4,10 +4,10 @@
 
 export TEST_MONGO_CONTAINER="mongoeye_mongo_test_server_1"
 export TEST_MONGO_HOST=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${TEST_MONGO_CONTAINER} 2>/dev/null`
-export TEST_MONGO_URI="${TEST_MONGO_HOST}:27017"
-export BENCHMARK_MONGO_URI="${TEST_MONGO_HOST}:27017"
-#export TEST_MONGO_URI="localhost:27017"
-#export BENCHMARK_MONGO_URI="localhost:27017"
+#export TEST_MONGO_URI="${TEST_MONGO_HOST}:27017"
+#export BENCHMARK_MONGO_URI="${TEST_MONGO_HOST}:27017"
+export TEST_MONGO_URI="localhost:27017"
+export BENCHMARK_MONGO_URI="localhost:27017"
 
 export BENCHMARK_DB="company"  # or people, company, restaurant, student
 export BENCHMARK_COL="company" # or people, company, restaurant, student
